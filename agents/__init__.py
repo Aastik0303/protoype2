f4212b80355b7073563bfe9f4e7ed2e506f215d3
Aastik Mishra <aastikmishra20@gmail.com>
2026-04-24 17:00:37 +0530

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,9 +1,9 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from .general import GeneralChatbotAgent
-from .code import CodeAgent
-from .document import DocumentRAGAgent
-from .youtube import YouTubeRAGAgent
-from .researcher import DeepResearcherAgent
-from .data_analyst import DataAnalystAgent
+import agents.general    as ag_general
+import agents.code       as ag_code
+import agents.document   as ag_document
+import agents.youtube    as ag_youtube
+import agents.researcher as ag_researcher
+import agents.data_analyst as ag_data
